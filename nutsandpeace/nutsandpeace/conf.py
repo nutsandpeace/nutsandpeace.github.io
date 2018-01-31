80250e70d8b7dcfc5234208e51e7897f3616cd10
vinodhmjhuman <vinodhmjhuman@gmail.com>
2018-01-31 23:30:31 +0530

    Nikola auto commit.
    Nikola version: 7.8.11

--- nutsandpeace/nutsandpeace/conf.py
+++ nutsandpeace/nutsandpeace/conf.py
@@ -17,7 +17,7 @@ import time
 
 
 # Data about this site
-BLOG_AUTHOR = "Kesari"  # (translatable)
+BLOG_AUTHOR = "Vishnu Menon"  # (translatable)
 BLOG_TITLE = "nutsandpeace"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
